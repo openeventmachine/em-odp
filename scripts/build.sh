15b8bbbdeaf903cb78ab2107ee578f1f4f992b77
Lifang Zhang <lifang.zhang@nokia.com>
2021-10-21 15:59:06 +0300

    scripts: clean up build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,31 +1,60 @@
 #!/bin/bash
-set -e
 
-#export ODP_VERSION=v1.23.0.0
-export ODP_VERSION=master
+set -o errexit # abort on nonzero exit status
+set -o nounset # abort on undeclared variable
 
-export BUILD_DIR=$(readlink -e $(dirname $0))/build
+make_doxygen=N
+while getopts ":d" opt; do
+  case "${opt}" in
+  d)
+    make_doxygen=Y
+    ;;
+  \?)
+    echo "Invalid option: -${OPTARG}" >&2
+    usage
+    exit 1
+    ;;
 
-mkdir ${BUILD_DIR} -p
-pushd ${BUILD_DIR}
-git clone --branch ${ODP_VERSION} --depth 1 https://github.com/OpenDataPlane/odp.git
-pushd ./odp
+  esac
+done
+
+#|-- em-odp (repository)
+#      |─ odp
+#      |─ installation
+#      └─ scripts
+script_dir=$(readlink --canonicalize "$(dirname "$0")")
+install_dir=$(dirname "${script_dir}")/installation
+mkdir "${install_dir}" --parents
+
+# Clone, build and install ODP which is needed by EM-ODP
+git clone --branch master --depth 1 https://github.com/OpenDataPlane/odp.git
+cd odp
 ./bootstrap
-./configure \
-	--prefix=${BUILD_DIR}/odp_install \
-	--without-examples \
-	--without-tests
-make -j $(nproc)
-make install
-popd && popd
 
-pushd ${BUILD_DIR}/../..
-./bootstrap
 ./configure \
-	--prefix=${BUILD_DIR}/em-odp_install \
-	--with-odp-path=${BUILD_DIR}/odp_install \
-	--enable-check-level=3 \
-	--enable-esv
-make -j $(nproc)
+  --prefix="${install_dir}/odp_install" \
+  --without-examples \
+  --without-tests
+
+make -j "$(nproc)"
 make install
-popd
+
+cd ..
+
+# Build and install EM-ODP
+./bootstrap
+
+./configure \
+  --prefix="${install_dir}/em-odp_install" \
+  --with-odp-path="${install_dir}/odp_install" \
+  --enable-check-level=3 \
+  --enable-esv \
+  CC="${CC}"
+
+# Make doxygen document
+if [[ "${make_doxygen}" = "Y" ]]; then
+  make doxygen-doc
+else
+  make -j "$(nproc)"
+  make install
+fi
